9baa9983b5175d8d2e50b1d15e7290ad2e09d7e0
byfyang <darkspherico@gmail.com>
2016-05-25 19:27:49 -0700

    json displays

--- starter-code/app.js
+++ starter-code/app.js
@@ -4,6 +4,8 @@ var path = require('path');
 var bodyParser = require('body-parser');
 var app = express();
 
+// helpers(app);
+
 // database setup
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/express-views-and-quotes');
@@ -15,8 +17,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static(__dirname + '/public'));
 
+app.set('views', path.join(__dirname, 'views'));
+app.engine('ejs', require('ejs').renderFile);
+app.set('view engine', 'ejs');
+
 // our routes
 var routes = require('./config/routes');
 app.use(routes);
 
-app.listen(3000);
+app.listen(5000);
